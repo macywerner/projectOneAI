d9f04669b2a7849aeb9bda4f1ddf2461cb048c4d
Colton Wedell <44662526+cwedell@users.noreply.github.com>
2021-09-28 17:25:43 -0700

    Updated DFS.cpp

--- DFS.cpp
+++ DFS.cpp
@@ -1,7 +1,15 @@
+/*
+Group members:
+Josh Michaels
+Colton Wedell
+Macy Werner
+*/
+
 #include <vector>
 #include <iostream>
 using namespace std;
 
+// An edge between two nodes: parent, child, and path cost
 class Edge {
 public:
   Edge(char p, char c, int w);
@@ -27,17 +35,18 @@ DFS::DFS() {
 
 }
 
+// Depth-First Search, takes a vector of edges
 void DFS::Search(vector<Edge> vec, int idx) {
   if(!vec.empty()) {
-    Edge e = vec[idx];
+    Edge e = vec[idx]; // grab first edge from list of all edges
     path.push_back(e);
-    if(e.child == 'g') {
+    if(e.child == 'g') { // if goal state is reached, return
       return;
-    } else if(e.child == ' ') {
+    } else if(e.child == ' ') { // if the node has no children, back up to parent of leaf node
       path.pop_back();
       path.pop_back();
       Search(vec, idx + 1);
-    } else {
+    } else { // continue for non-leaf nodes
       Search(vec, idx + 1);
     }
   }
@@ -45,6 +54,7 @@ void DFS::Search(vector<Edge> vec, int idx) {
 
 int main(int argc, char** argv) {
   vector<Edge> myEdges;
+  // add all edges to the graph
   myEdges.push_back(Edge('s', 'a', 3));
   myEdges.push_back(Edge('a', 'd', 3));
   myEdges.push_back(Edge('d', ' ', 0));
@@ -62,6 +72,7 @@ int main(int argc, char** argv) {
 
   int cost = 0;
 
+  // Prints formatted path with edge cost
   cout << "Path found:" << endl;
   for(int i = 0; i < mySearch.path.size(); ++i) {
     cost += mySearch.path[i].weight;
@@ -74,33 +85,3 @@ int main(int argc, char** argv) {
   }
   cout << "\nCost: " << cost << endl;
 }
-
-/*
-class Node {
-public:
-  Node(state, parent[]=NULL, path_cost=0, children[]=NULL);
-
-private:
-  Node[] myparents;
-  Node[] mychildren;
-  int[] mypath_costs;
-}
-
-Node::Node(state, parents[]=NULL, path_costs[]=0, children[]=NULL) {
-  myparents[] = parent;
-  mychildren[] = children;
-  mypath_costs[] = path_costs;
-}
-
-class Graph {
-public:
-  Graph(nodes[]=NULL);
-
-private:
-  Node[] mynodes;
-}
-
-Graph::Graph(nodes[]) {
-  mynodes[] = nodes;
-}
-*/
